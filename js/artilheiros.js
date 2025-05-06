1318d9b2ffa505523949dacd81d63884e592bbbf
Johansson Lucena <105368743+johanssonlucena@users.noreply.github.com>
2025-05-06 17:38:41 -0300

    Update artilheiros.js

--- js/artilheiros.js
+++ js/artilheiros.js
@@ -1,23 +1,27 @@
 const artilheiros = [
-  { "nome": "Hitalo", "gols": 13 },
+  { "nome": "Hitalo", "gols": 15 },
+  { "nome": "Rondinely", "gols": 14 },
+  { "nome": "Augustinho", "gols": 12 },
+  { "nome": "Preto", "gols": 11 },
   { "nome": "Roberio", "gols": 11 },
-  { "nome": "Rondinely", "gols": 10 },
-  { "nome": "Augustinho", "gols": 9 },
-  { "nome": "Preto", "gols": 6 },
+  { "nome": "Joalisson", "gols": 8 }, 
   { "nome": "Wellington", "gols": 5 },
   { "nome": "Emerson", "gols": 5 },
   { "nome": "Jones", "gols": 5 },
+  { "nome": "Alysson", "gols": 5 },
   { "nome": "Michelly", "gols": 4 },
   { "nome": "Bruno Daniel", "gols": 4 },
-  { "nome": "Joalisson", "gols": 3 },
   { "nome": "Kerles", "gols": 3 },
+  { "nome": "Ricardo", "gols": 2 },
   { "nome": "Raiff", "gols": 2 },
+  { "nome": "Julian", "gols": 2 },
   { "nome": "Junior Piscina", "gols": 2 },
-  { "nome": "Julian", "gols": 1 },
+  { "nome": "Johansson", "gols": 1 },
+  { "nome": "Indio", "gols": 1 }
+  { "nome": "Afonso", "gols": 1 }
   { "nome": "Lindomar", "gols": 1 },
   { "nome": "Oliveira", "gols": 1 },
   { "nome": "Ranny", "gols": 1 },
-  { "nome": "Ricardo", "gols": 1 },
-  { "nome": "Alysson", "gols": 1 },
-  { "nome": "Indio", "gols": 1 }
+  { "nome": "Roquelan", "gols": 1 },
+  { "nome": "Tau", "gols": 1 }
 ];
