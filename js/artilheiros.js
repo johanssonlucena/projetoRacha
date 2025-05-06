062798d3f3a32d5a656cd615b716f8ce0f126e3a
Johansson Lucena <105368743+johanssonlucena@users.noreply.github.com>
2025-05-06 18:08:18 -0300

    Update artilheiros.js

--- js/artilheiros.js
+++ js/artilheiros.js
@@ -17,8 +17,8 @@ const artilheiros = [
   { "nome": "Julian", "gols": 2 },
   { "nome": "Junior Piscina", "gols": 2 },
   { "nome": "Johansson", "gols": 1 },
-  { "nome": "Indio", "gols": 1 }
-  { "nome": "Afonso", "gols": 1 }
+  { "nome": "Indio", "gols": 1 },
+  { "nome": "Afonso", "gols": 1 },
   { "nome": "Lindomar", "gols": 1 },
   { "nome": "Oliveira", "gols": 1 },
   { "nome": "Ranny", "gols": 1 },
